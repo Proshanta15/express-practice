aeae00d6eca5d8b802a7d377b1a231fc515fee60
Proshanta Kumar Roy <Proshanta.ict@pakiza.com>
2026-02-17 14:50:36 +0600

    node

--- mongoose/mongoose.test.js
+++ mongoose/mongoose.test.js
@@ -1,9 +1,27 @@
 import mongoose from "mongoose";
 
 try {
-    await mongoose.connect("mongodb://localhost:27017/mongodb_nodejs_db");
+    await mongoose.connect("mongodb://127.0.0.1/mongoose_middleware");
     mongoose.set("debug", true);
 } catch (error) {
     console.error(error);
     process.exit();
 }
+
+const userSchema = mongoose.Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    age: {type: Number, required: true, min: 5},
+    // createdAt: {type: Date, default: Date.now()},
+    // updatedAt: {type: Date, default: Date.now()}
+},{
+    timestamps: true
+});
+
+const Users = mongoose.model("user", userSchema);
+
+// await Users.create({name: "Proshanta", age: 30, email: "roy@gmail.com"});
+await Users.updateOne({ email: "roy@gmail.com"}, {$set: {age: 30}});
+
+
+await mongoose.connection.close();  
